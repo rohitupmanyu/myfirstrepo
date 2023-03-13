1b764d85d07165a9cd4670cb5a1b34ec82a8e035
unknown <upmanyu.rohit@gmail.com>
2023-03-13 22:53:56 +0530

    Using short declaration operator

---
+++ shortdeclarationoperator.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42 //declare and assign at the same time
+	fmt.Println(x)
+	x = 99 // Just assign as the variable is already declared with "short declaration operator"
+	fmt.Println(x)
+	y := 100 + 24
+	fmt.Println(y)
+	
+}
