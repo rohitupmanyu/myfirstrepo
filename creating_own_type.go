bb23f9092bbddd22e31c354a69f571a603440e32
unknown <upmanyu.rohit@gmail.com>
2023-03-14 22:45:04 +0530

    Create my own type

---
+++ creating_own_type.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+var a int
+type hotdog int //Isn't this similar to C typedefs
+var b hotdog
+
+func main() {
+	a = 42
+	b = 43
+	fmt.Println(a)
+	fmt.Printf("%T\n", a)
+	fmt.Println(b)
+	fmt.Printf("%T\n", b)
+}
